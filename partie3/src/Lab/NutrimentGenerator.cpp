76fef4059cc1354e8a71c26ccdcadb0fd28161e6
David <david.nigri@epfl.ch>
2020-03-31 14:10:21 +0200

    Etape 3.1 FinA

--- partie3/src/Lab/NutrimentGenerator.cpp
+++ partie3/src/Lab/NutrimentGenerator.cpp
@@ -14,24 +14,25 @@ NutrimentGenerator::NutrimentGenerator():
 
 void NutrimentGenerator::update(sf::Time dt){
     TimeLastGen+=dt;
+    Nutriment* pointNut;
     if(TimeLastGen > sf::seconds(getShortConfig().generator_nutriment_delay)){
         TimeLastGen=sf::Time::Zero;
         Vec2d tailleEnv(getApp().getLabSize());
         double posX(normal(tailleEnv.x/2, tailleEnv.x*tailleEnv.x/16));
         double posY(normal(tailleEnv.y/2, tailleEnv.y*tailleEnv.y/16));
-//        Vec2d pos(normal(tailleEnv/2, tailleEnv*tailleEnv/16));
         Vec2d pos(posX,posY);
+//        bool test(bernoulli(getShortConfig().generator_nutriment_prob ));
         if(bernoulli(getShortConfig().generator_nutriment_prob )){
             Quantity qt(uniform(getShortConfig().nutrimentA_min_qty,getShortConfig().nutrimentA_max_qty));
-            NutrimentA nut(qt,pos);
-            getAppEnv().addNutriment(&nut);
+            pointNut=new NutrimentA(qt,pos);
+            getAppEnv().addNutriment(pointNut);
         }
         else {
             Quantity qt(uniform(getShortConfig().nutrimentB_min_qty,getShortConfig().nutrimentB_max_qty));
-            NutrimentB nut(qt,pos);
-            getAppEnv().addNutriment(&nut);
+//            NutrimentB nut(qt,pos);
+            pointNut=new NutrimentB(qt,pos);
+            getAppEnv().addNutriment(pointNut);
         }
-
     }
 }
 
