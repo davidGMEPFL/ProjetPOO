d984611512b59cec1249d2d0ff019faf50a0b78c
David <david.nigri@epfl.ch>
2020-05-06 18:28:31 +0200

    sans SegFault

--- partie5/src/Stats/Stats.cpp
+++ partie5/src/Stats/Stats.cpp
@@ -9,7 +9,9 @@ void Stats::setActive(const int& id){
 
 
 std::string Stats::getCurrentTitle(){
-    return Libelles[activeID];
+    if(!Libelles.empty())
+        return Libelles[activeID];
+    else return "";
 }
 
 void Stats::next(){
@@ -37,8 +39,9 @@ void Stats::drawOn(sf::RenderTarget& TargetWindow){
 
 void Stats::addGraph(int idGraph, std::string const& titreGraph,
                      std::vector<std::string> sesLibelles, double min, double max, Vec2d const& size){
-    Graphs[idGraph].reset();
-    Graphs[idGraph]=unique_ptr<Graph>(new Graph(sesLibelles, size,  min,  max));
-    Libelles[idGraph]=titreGraph;
-    activeID=idGraph;
+//    if (Graphs[idGraph])
+//    Graphs[idGraph].reset();
+//    Graphs[idGraph]=unique_ptr<Graph>(new Graph(sesLibelles, size,  min,  max));
+//    Libelles[idGraph]=titreGraph;
+//    activeID=idGraph;
 }
